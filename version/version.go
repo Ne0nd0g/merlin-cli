95dceb1a89871bac0cf17a0ccdd148c57a530ea1
Russel Van Tuyl <russel.vantuyl@gmail.com>
2023-10-29 16:20:58 -0400

    Updated version

--- version/version.go
+++ version/version.go
@@ -21,7 +21,7 @@ along with Merlin.  If not, see <http://www.gnu.org/licenses/>.
 package version
 
 // Version is a constant variable containing the version number for the Merlin CLI program
-const Version = "0.0.0"
+const Version = "1.0.0"
 
 // Build is the unique number based off the git commit in which it is compiled against
 var Build = "nonRelease"
